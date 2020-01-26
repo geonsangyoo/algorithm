c2fd257b122392aa41e6e17e753f645487f39596
geonsangyoo <geonsangyoo@gmail.com>
2020-01-26 15:09:55 +0900

    [Algorithm][#2026]-excursion-1st commit-20200126

--- #9465/code2.py
+++ #9465/code2.py
@@ -7,8 +7,8 @@ else:
     f=sys.stdin
 def find():
     for i in range(2, column_num+1):
-        b[2]=max(c[1]+num[0][i-1],b[0]+num[0][i-1]+num[1][i-2],c[0]+num[0][i-1])
-        c[2]=max(b[1]+num[1][i-1],c[0]+num[1][i-1]+num[0][i-2],b[0]+num[1][i-1])
+        b[2]=max(c[1]+num[0][i-1] , b[0]+num[0][i-1]+num[1][i-2] , c[0]+num[0][i-1])
+        c[2]=max(b[1]+num[1][i-1] , c[0]+num[1][i-1]+num[0][i-2] , b[0]+num[1][i-1])
         b[0]=b[1];b[1]=b[2]
         c[0]=c[1];c[1]=c[2]
 N=int(f.readline().strip())
