a7fe3269951244d1f256c60e5cecb2279d6e7d73
geonsangyoo <geonsangyoo@gmail.com>
2020-07-05 14:30:45 +0900

    [Algorithm][AtCoder#172]-Tsundoku-20200705_01

---
+++ AtCoder/202006_5th/C2.py
@@ -0,0 +1,31 @@
+import sys
+DEBUG = True
+# DEBUG = False
+if DEBUG:
+    f=open("202006_5th/C_input.txt")
+else:
+    f=sys.stdin
+#----
+M,N,K=map(int,f.readline().split())
+ans=0
+__=N
+M_shelf=list(map(int,f.readline().split()))
+N_shelf=list(map(int,f.readline().split()))
+M_partial=[]
+N_partial=[]
+M_partial.append(0)
+N_partial.append(0)
+#----
+for _ in range(M):
+    M_partial.append(M_partial[_]+M_shelf[_])
+for _ in range(N):
+    N_partial.append(N_partial[_]+N_shelf[_])
+for _ in range(1,M+1):
+    R=K-M_partial[_]
+    if R<0:
+        continue
+    while R<N_partial[__]:
+        __-=1
+    ans=max(ans,_+__)
+print(ans)
+f.close()
