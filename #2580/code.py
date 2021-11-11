04e67328a1ea46602c122ad58d030d1c1e321401
Geonsang Yoo <geonsangyoo@gmail.com>
2021-11-11 17:27:52 +0900

    problem #2580 is updated

--- #2580/code.py
+++ #2580/code.py
@@ -8,55 +8,26 @@ if DEBUG:
 else:
   f = sys.stdin
 
-# N = 9
-
 # SUDOKU_MAP insertion
 SUDOKU_MAP = [list(map(int, f.readline().split())) for _ in range(9)]
 
-# Q insertion
-Q = [(_, __) for __ in range(9) for _ in range(9) if SUDOKU_MAP[_][__] == 0]
-# Q = []
-# for _ in range(9):
-#   for __ in range(9):
-#     if SUDOKU_MAP[_][__] == 0:
-#       Q.append((_, __))
+candidates = [(0, 0), (0, 3), (0, 6), (3, 0), (3, 3), (3, 6), (6, 0), (6, 3), (6, 6)]
 
-# Check if the number inserted meets the rules of SUDOKU
-# def check(x, y, v):
-#   for _ in range(N):
-#     if SUDOKU_MAP[_][y] == v and _ != x:
-#       return False
+ROWMAP = [[False for __ in range(9)] for _ in range(9)]
+COLMAP = [[False for __ in range(9)] for _ in range(9)]
+RECMAP = [[False for __ in range(9)] for _ in range(9)]
 
-#   for _ in range(N):
-#     if SUDOKU_MAP[x][_] == v and _ != y:
-#       return False
+Q = []
 
-#   for _ in range(x // 3 * 3, x // 3 * 3 + 3):
-#     for __ in range(y // 3 * 3, y // 3 * 3 + 3):
-#       if SUDOKU_MAP[_][__] == v and _ != x and __ != y:
-#         return False
-
-#   return True
-
-def get_candidates(x, y):
-  numbers = [i + 1 for i in range(9)]
-  xs = x // 3 * 3
-  xe = xs + 3
-  ys = y // 3 * 3
-  ye = ys + 3
-
-  for _ in range(9):
-    if SUDOKU_MAP[x][_] in numbers:
-      numbers.remove(SUDOKU_MAP[x][_])
-    if SUDOKU_MAP[_][y] in numbers:
-      numbers.remove(SUDOKU_MAP[_][y])
-
-  for _ in range(xs, xe):
-    for __ in range(ys, ye):
-      if SUDOKU_MAP[_][__] in numbers:
-        numbers.remove(SUDOKU_MAP[_][__])
-    
-  return numbers
+for _ in range(9):
+  for __ in range(9):
+    if SUDOKU_MAP[_][__] != 0:
+      ROWMAP[_][SUDOKU_MAP[_][__] - 1] = True
+      COLMAP[__][SUDOKU_MAP[_][__] - 1] = True
+      RECMAP[candidates.index((_ // 3 * 3, __ // 3 * 3))][SUDOKU_MAP[_][__] - 1] = True
+    else:
+      # Q insertion
+      Q.append((_, __))
 
 # Put a new number
 def put_number(idx):
@@ -66,17 +37,21 @@ def put_number(idx):
     exit()
 
   x, y = Q[idx]
-  candidates = get_candidates(x, y)
+  
+  xs = x // 3 * 3
+  ys = y // 3 * 3
+  i = candidates.index((xs, ys))
 
-  for v in candidates:
-    SUDOKU_MAP[x][y] = v
-    put_number(idx + 1)
-    SUDOKU_MAP[x][y] = 0
-
-  # for v in range(1, N + 1):
-  #   SUDOKU_MAP[x][y] = v
-  #   if check(x, y, v) == True:
-  #     put_number(idx + 1)
-  #     SUDOKU_MAP[x][y] = 0
+  for v in range(1, 10):
+    if not ROWMAP[x][v - 1] and not COLMAP[y][v - 1] and not RECMAP[i][v - 1]:
+      SUDOKU_MAP[x][y] = v
+      ROWMAP[x][v - 1] = True
+      COLMAP[y][v - 1] = True
+      RECMAP[i][v - 1] = True
+      put_number(idx + 1)
+      SUDOKU_MAP[x][y] = 0
+      ROWMAP[x][v - 1] = False
+      COLMAP[y][v - 1] = False
+      RECMAP[i][v - 1] = False
 
 put_number(0)
